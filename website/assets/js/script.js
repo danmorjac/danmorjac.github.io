dd2d761fbb3901ceb7d9bde7aef206202228e783
danmorjac <danielmoralesja@gmail.com>
2024-01-31 23:17:17 +0100

    update: images and css for the header and images

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -1,16 +1,34 @@
-document.querySelectorAll('.languajes-tools').forEach(function (element) {
-  // Ocultar todos los elementos con la clase hidden-cont al principio
-  let targetId = element.nextElementSibling.id;
-  let targetElement = document.getElementById(targetId);
-  targetElement.style.display = 'none';
+// Selecciona todos los elementos con la clase 'languajes-tools'
+let elements = document.querySelectorAll('.languajes-tools');
 
+// Itera sobre cada elemento seleccionado
+elements.forEach(function (element, index) {
+  // Verifica si el elemento no es el primero
+  if (index !== 0) {
+    // Oculta el elemento correspondiente
+    let targetId = element.nextElementSibling.id;
+    let targetElement = document.getElementById(targetId);
+    targetElement.style.display = 'none';
+  }
+
+  // Agrega un evento 'click' a cada elemento
   element.addEventListener('click', function () {
-    // Ocultar todos los elementos con la clase hidden-cont
+    // Oculta todos los elementos con la clase 'hidden-cont'
     document.querySelectorAll('.hidden-cont').forEach(function (hiddenElement) {
       hiddenElement.style.display = 'none';
     });
 
-    // Mostrar u ocultar el elemento actual
+    // Muestra u oculta el elemento actual al que se hizo clic
+    let targetId = element.nextElementSibling.id;
+    let targetElement = document.getElementById(targetId);
     targetElement.style.display = (targetElement.style.display === 'none' || targetElement.style.display === '') ? 'block' : 'none';
+
+    // Remueve la clase 'active' de todos los elementos
+    elements.forEach(function (el) {
+      el.classList.remove('active');
+    });
+
+    // Agrega la clase 'active' al elemento que se hizo clic
+    element.classList.add('active');
   });
 });
